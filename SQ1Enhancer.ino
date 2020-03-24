57a9ce205fffe01721226a4a73b042eb1cfcd827
Karsten Gorkow <karsten.gorkow@me.com>
2020-03-24 17:12:13 +0100

    first free

--- SQ1Enhancer.ino
+++ SQ1Enhancer.ino
@@ -8,7 +8,8 @@ int thresholdPin = A1; // input pin for the threshold pot
 int swingPotPin = A2; // input pin for the swing pot
 int ledPin = LED_BUILTIN;      // select the pin for the LED
 int syncOutPin = 2;  
-int settingsBtnInPin = 3; // input for button to switch sync resolution
+int skipsBtnInPin = 3; // input for button to switch skips
+int subsBtnInPin = 4; // input for button to switch subs
 
 int audioValue = 0;  // variable to store the value coming from the audio in
 int thresholdValue = 0;
@@ -24,111 +25,85 @@ int outputPulseTime = 15;
 bool stopOnNoPulse = true;
 int stopDelay = 3000;
 
-int state = HIGH;      // the current toggled state of the subDivBtn
-int reading;           // the current reading from the settingsBtnInPin pin
-int previous = LOW;    // the previous reading from the settingsBtnInPin pin
-unsigned long time = 0;         // the last time the output settingsBtnInPin was toggled
-int debounce = 200;   // the debounce time, increase if the output flickers
+struct ToggleButton {
+  int inPin;
+  int state;      // the current toggled state of the button
+  int reading;           // the current reading from the button's pin
+  int previous;    // the previous reading from the button's pin
+  unsigned long time = 0;  // the last time the output was toggled
+};
 
-float fac[] = {4.0f, 3.0f, 2.0f, 1.0f+1.0f/3.0f, 1.0f, 0.5f, 1.0f/3.0f, 0.25f, 1.0f/6.0f, 0.125f};
-int skips[] =   {3,    2,    1,    1,              0,    0,    0,         0,     0,         0     };
-int subs[] =    {0,    0,    0,    0,              0,    1,    2,         3,     5,         7     };
-int setIndexDef = 5;
-int setIndex = -1;
+const int buttonDebounceTime = 200;
+
+ToggleButton skipsBtn;
+ToggleButton subsBtn;
+
+const int maxSkips = 7;
+const int maxSubs = 7;
+
+int skips = 0;
+int subs = 1;
 int skipCounter;
 int subCounter;
 
-void pulseOutput();
-void checkPulse();
+void setupToggleButton(int pin, ToggleButton* pbtn) {
+  pbtn->inPin = pin;
+  pbtn->state = HIGH;
+  pbtn->previous = LOW;
+  pbtn->time = 0;
+  pinMode(pbtn->inPin, INPUT);
+}
 
 void setup() {
   // declare the ledPin as an OUTPUT:
   pinMode(ledPin, OUTPUT);
-  pinMode(syncOutPin, OUTPUT);
-  pinMode(settingsBtnInPin, INPUT);
+  setupToggleButton(3, &skipsBtn);
+  setupToggleButton(4, &subsBtn);
   Serial.begin(9600);
 }
 
-bool setSetIndex() {
-  bool oldState = state;
-  bool changeForced = setIndex < 0;
-  if (changeForced) {
-    setIndex = setIndexDef;
-  }
-  reading = digitalRead(settingsBtnInPin);
+void printInfo() {
+  Serial.print("bpm=\t");
+  Serial.print(60000 / pulseRate);
+  Serial.print("\tskips=\t");
+  Serial.print(skips);
+  Serial.print("\tcnt=\t");
+  Serial.print(skipCounter);
+  Serial.print("\tsubs=\t");
+  Serial.print(subs);
+  Serial.print("\tcnt=\t");
+  Serial.print(subCounter);
+  Serial.print("\tfac=\t");
+  Serial.println(currentFac());
+}
+
+bool setStepVal(ToggleButton* pbtn, int* val, int minVal, int maxVal) {
+  if (*val < minVal) *val = minVal;
+  if (*val > maxVal) *val = maxVal;
+  bool oldState = pbtn->state;
+  pbtn->reading = digitalRead(pbtn->inPin);
   // if the input just went from LOW and HIGH and we've waited long enough
   // to ignore any noise on the circuit, toggle the output pin and remember
   // the time
-  if (reading == HIGH && previous == LOW && millis() - time > debounce) {
-    if (state == HIGH)
-      state = LOW;
+  if (pbtn->reading == HIGH && pbtn->previous == LOW && millis() - pbtn->time > buttonDebounceTime) {
+    if (pbtn->state == HIGH)
+      pbtn->state = LOW;
     else
-      state = HIGH;
+      pbtn->state = HIGH;
 
-    setIndex++;
-    if (setIndex >= sizeof(fac) / sizeof(fac[0])) {
-      setIndex = 0;
+    (*val)++;
+    if (*val > maxVal) {
+      skips = minVal;
     }
-    time = millis();    
+    pbtn->time = millis();    
   }
 
-  previous = reading;
-  return oldState != state || changeForced;
+  pbtn->previous = pbtn->reading;
+  return oldState != pbtn->state;
 }
 
 float currentFac() {
-  return fac[setIndex];
-}
-
-int currentSkips() {
-  return skips[setIndex];
-}
-
-int currentSubs() {
-  return subs[setIndex];
-}
-
-void loop() {
-  audioValue = analogRead(audioPin);
-  thresholdValue = analogRead(thresholdPin);
-  bool subDivsChanged = setSetIndex();
-  checkPulse();
-  if (pulseRateSet && millis() >= nextPulseTS) {
-    if (currentSubs() != subCounter) {
-      pulseOutput();
-      subCounter++;
-      nextPulseTS = lastPulseTS + (unsigned long)((subCounter * currentFac() * pulseRate) + .5f);
-    }
-  }
-}
-
-void checkPulse() {
-  if (audioValue > thresholdValue) {
-    thisPulseTS = millis();
-    if (!firstPulseSeen) {
-      firstPulseSeen = true;
-      lastPulseTS = thisPulseTS;
-      skipCounter = currentSkips();
-    }
-    else if ((thisPulseTS - lastPulseTS) > minDeltaPulseTS) {
-      pulseRate = thisPulseTS - lastPulseTS;
-      lastPulseTS = thisPulseTS;
-      pulseRateSet = true;
-      subCounter = 0;
-      if (skipCounter <= 0) {
-        skipCounter = currentSkips();
-        pulseOutput();
-        nextPulseTS = thisPulseTS + ((pulseRate * currentFac()) + 0.5f);
-      }
-      else {
-        --skipCounter;
-      }
-    }
-  }
-  else if (firstPulseSeen && stopOnNoPulse && (millis() - lastPulseTS) > stopDelay) {
-    firstPulseSeen = false;
-    pulseRateSet = false;
-  }
+  return ((float)(skips + 1)) / ((float) (subs + 1));
 }
 
 void pulseOutput() {
@@ -137,8 +112,47 @@ void pulseOutput() {
   delay(outputPulseTime);
   digitalWrite(ledPin, LOW);
   digitalWrite(syncOutPin, LOW);
-  Serial.print("bpm=\t");
-  Serial.print(60000 / pulseRate);
-  Serial.print("\tsubdiv=\t");
-  Serial.println(currentFac());
+}
+
+void checkPulse() {
+  if (audioValue > thresholdValue) {
+    thisPulseTS = millis();
+    if (!firstPulseSeen) {
+      firstPulseSeen = true;
+      lastPulseTS = thisPulseTS;
+      skipCounter = skips;
+    }
+    else if ((thisPulseTS - lastPulseTS) > minDeltaPulseTS) {
+      pulseRate = thisPulseTS - lastPulseTS;
+      lastPulseTS = thisPulseTS;
+      pulseRateSet = true;
+      subCounter = 0;
+      if (skipCounter <= 0) {
+        skipCounter = skips;
+        pulseOutput();
+        nextPulseTS = thisPulseTS + ((pulseRate * currentFac()) + 0.5f);
+      }
+      else {
+        --skipCounter;
+      }
+      printInfo();
+    }
+  }
+  else if (firstPulseSeen && stopOnNoPulse && (millis() - lastPulseTS) > stopDelay) {
+    firstPulseSeen = false;
+    pulseRateSet = false;
+  }
+}
+
+void loop() {
+  audioValue = analogRead(audioPin);
+  thresholdValue = analogRead(thresholdPin);
+  setStepVal(&skipsBtn, &skips, 0, maxSkips);
+  setStepVal(&subsBtn, &subs, 0, maxSubs);
+  checkPulse();
+  if (pulseRateSet && millis() >= nextPulseTS && subs != subCounter) {
+    pulseOutput();
+    subCounter++;
+    nextPulseTS = lastPulseTS + (unsigned long)((subCounter * currentFac() * pulseRate) + .5f);
+  }
 }
